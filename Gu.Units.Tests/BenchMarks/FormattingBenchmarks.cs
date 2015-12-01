7346839c5fd9cdf63b245454064786e920fddd5d
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-01 01:59:38 +0100

    Fix formatting tests.

--- Gu.Units.Tests/BenchMarks/FormattingBenchmarks.cs
+++ Gu.Units.Tests/BenchMarks/FormattingBenchmarks.cs
@@ -17,27 +17,53 @@
             // this is not a fair comparison as .ToString parses the arguments. Still interesting as a base line.
             var speed = Speed.FromMetresPerSecond(1.2);
             var toString = speed.ToString("F1 m/s");
+            toString = speed.ToString("F0", "s⁻¹⋅mm");
+            toString = speed.ToString("F2 m⋅s⁻¹");
             var string_Format = $"{speed.metresPerSecond:F1} {SpeedUnit.MetresPerSecond}";
             // end warmup
 
             var sw = Stopwatch.StartNew();
             var n = 1000000;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n / 3; i++)
             {
                 toString = speed.ToString("F1 m/s");
+                toString = speed.ToString("F0", "s⁻¹⋅mm");
+                toString = speed.ToString("F2 m⋅s⁻¹");
             }
 
             sw.Stop();
             Console.WriteLine($"// {DateTime.Today.ToShortDateString()}| speed.ToString(\"F1 m/s\")                                   {n:N0} times took: {sw.ElapsedMilliseconds} ms");
 
             sw.Restart();
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n / 3; i++)
             {
                 string_Format = $"{speed.metresPerSecond:F1} {SpeedUnit.MetresPerSecond}";
+                string_Format = $"{speed.metresPerSecond:F0} s⁻¹⋅mm";
+                string_Format = $"{speed.metresPerSecond:F2} m⋅s⁻¹";
             }
 
             sw.Stop();
             Console.WriteLine($"// {DateTime.Today.ToShortDateString()}| $\"{{ speed.metresPerSecond:F1}} {{SpeedUnit.MetresPerSecond}}\" {n:N0} times took: {sw.ElapsedMilliseconds} ms");
+        }
+
+        [Test]
+        public void ForProfiling()
+        {
+            // this is not a fair comparison as .ToString parses the arguments. Still interesting as a base line.
+            var speed = Speed.FromMetresPerSecond(1.2);
+            var toString = speed.ToString("F1 m/s");
+            toString = speed.ToString("F0", "s⁻¹⋅mm");
+            toString = speed.ToString("F2 m⋅s⁻¹");
+            var string_Format = $"{speed.metresPerSecond:F1} {SpeedUnit.MetresPerSecond}";
+            // end warmup
+
+            var n = 1000000;
+            for (int i = 0; i < n/3; i++)
+            {
+                toString = speed.ToString("F1 m/s");
+                toString = speed.ToString("F0", "s⁻¹⋅mm");
+                toString = speed.ToString("F2 m⋅s⁻¹");
+            }
         }
     }
 }
