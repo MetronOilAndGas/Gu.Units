5b556bdd1af8217caf42ebcd734efed4988f8700
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-29 10:35:09 +0100

    Before generation of ValueConverters

--- Gu.Units.Wpf/LengthConverter.generated.cs
+++ Gu.Units.Wpf/LengthConverter.generated.cs
@@ -250,7 +250,13 @@
                             return new Length(d, this.unit.Value);
                         }
 
-                        return value; // returning raw to trigger error
+                        Length result;
+                        if (Length.TryParse(text, NumberStyles.Float, culture, out result))
+                        {
+                            return $"#{text}#"; // returning modified text so that TypeConverter fails and we get an error
+                        }
+
+                        return text; // returning raw to trigger error
                     }
                 case Wpf.UnitInput.SymbolAllowed:
                     {
